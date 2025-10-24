3a81c3e7b7079af5f631da9abe0f0f47a281c533
Jasmine <jasminetntu@gmail.com>
2025-10-24 13:22:04 -0700

    user interface work flow

--- src/main/java/com/cardealership/UserInterface.java
+++ src/main/java/com/cardealership/UserInterface.java
@@ -22,9 +22,9 @@ public class UserInterface {
             choice = scnr.nextLine().trim().toLowerCase();
 
             switch (choice) {
-                case "1" -> processGetAllVehiclesRequest();
-                case "2" -> processAddVehicleRequest();
-                case "3" -> processRemoveVehicleRequest();
+                case "1" -> processAddVehicleRequest();
+                case "2" -> processRemoveVehicleRequest();
+                case "3" -> processGetAllVehiclesRequest();
                 case "4" -> processGetByPriceRequest();
                 case "5" -> processGetByMakeModelRequest();
                 case "6" -> processGetByYearRequest();
@@ -43,65 +43,68 @@ public class UserInterface {
 
     // *** General Processing Methods ***
 
-    public void processGetAllVehiclesRequest() {
-
-    }
-
     public void processAddVehicleRequest() {
-
+        System.out.println("Add vehicle");
     }
 
     public void processRemoveVehicleRequest() {
+        System.out.println("Remove vehicle");
+    }
 
+    public void processGetAllVehiclesRequest() {
+        System.out.println("Get all vehicles");
     }
 
     // *** Get By Processing Methods ***
 
     public void processGetByPriceRequest() {
-
+        System.out.println("Get by price");
     }
 
     public void processGetByMakeModelRequest() {
-
+        System.out.println("Get by make model");
     }
 
     public void processGetByYearRequest() {
-
+        System.out.println("Get by year");
     }
 
     public void processGetByColorRequest() {
-
+        System.out.println("Get by color");
     }
 
     public void processGetByMileageRequest() {
-
+        System.out.println("Get by mileage");
     }
 
     public void processGetByVehicleTypeRequest() {
-
+        System.out.println("Get by vehicle type");
     }
 
     // *** Helper Methods ***
+
     private void init() {
         DealershipFileManager dsm = new DealershipFileManager();
         dealership = dsm.getDealership();
     }
 
     private void displayMenu() {
-        System.out.println("""
-                🛞 What would you like to do?
-                    [1] View all vehicles
-                    [2]
-                    [3]
-                    [4]
-                    [5]
-                    [6]
-                    [7]
-                    [8]
-                    [9]
-                    
+        System.out.print("""
+                ===================================
+                | 🛞 What would you like to do?   |
+                ===================================
+                    [1] Add vehicle
+                    [2] Remove vehicle
+                    [3] View all vehicles
+                    [4] View by price
+                    [5] View by Make & Model
+                    [6] View by Year
+                    [7] View by Color
+                    [8] View by Mileage
+                    [9] View by Vehicle Type
+                
                     [X] Exit
-                    """);
+                Enter your choice (1-9, X):\s""");
     }
 
     private void displayVehicles(List<Vehicle> vehicleList) {
