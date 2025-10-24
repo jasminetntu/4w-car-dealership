0dfc5adf35e255cacf76817536db94231549085b
Jasmine <jasminetntu@gmail.com>
2025-10-24 10:45:19 -0700

    transfer work from java academy to this repo

---
+++ src/main/java/com/cardealership/Program.java
@@ -0,0 +1,11 @@
+package com.cardealership;
+
+public class Program {
+    public static void main(String[] args) {
+        //setup
+        UserInterface ui = new UserInterface();
+
+        //enter app
+        ui.display();
+    }
+}
